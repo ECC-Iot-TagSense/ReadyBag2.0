0b920aa2528d5ccc58c3ea2c2fd26661c1c15ec3
Rate980 <xv0nnj0@icloud.com>
2024-11-03 16:28:52 +0900

    Fix ヘッダーから処理を追い出した

--- src/index.h
+++ src/index.h
@@ -1,28 +1,6 @@
 #ifndef __index_h__
 #define __index_h__
 
-int updaterIndex(int currentIndex, int maxIndex, int direction)
-{
-    auto diff = 0;
-    if (direction < 0)
-    {
-        diff = -1;
-    }
-    if (direction > 0)
-    {
-        diff = 1;
-    }
-    auto nextIndex = currentIndex + diff;
-
-    if (nextIndex < 0)
-    {
-        nextIndex = maxIndex;
-    }
-    if (nextIndex > maxIndex)
-    {
-        nextIndex = 0;
-    }
-    return nextIndex;
-}
+int updaterIndex(int currentIndex, int maxIndex, int direction);
 
 #endif
