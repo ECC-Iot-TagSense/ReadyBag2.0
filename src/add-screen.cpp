c468207ce996a760e793b2fb97ab8fa7c8cbcaab
Rate980 <2220208@ecc.ac.jp>
2024-11-07 19:18:44 +0900

    Add ステータス

--- src/add-screen.cpp
+++ src/add-screen.cpp
@@ -1,4 +1,18 @@
 #include "add-screen.h"
+#include "index.h"
+namespace AddScreen
+{
+    enum class State
+    {
+        Select,
+        Scan,
+        Done,
+    };
+    int waitTime = 0;
+    State state = State::Select;
+    int selectionIndex = 0;
+} // namespace AddScreen
+  //
 
 ScreenState addLoop(M5GFX *display, Encoder *encoder, Reader *reader, m5::Button_Class *button, bool isFirst)
 {
